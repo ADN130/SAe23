f3740c27ee13863918e4f8f779870761fa25f3d2
ADN <adnane.bensaid.iut@gmail.com>
2023-06-03 20:24:09 +0200

    envoi du script métriques dynamique

--- Script/script.sh
+++ Script/script.sh
@@ -5,74 +5,44 @@
 while true
 do
 
+#--------------------------------------SCRIPT DYNAMIQUE-----------------------------------------#
 
-# Subscription to building B
-
-abo_B113=$(mosquitto_sub -h mqtt.iut-blagnac.fr -t "Student/by-room/B113/data" -C 1) #subscription to the topic
-temp=20 #getting the temperature
-hum=1 #same thing for the humidity
-date=$(date +%F) #getting the date
-heure=$(date +%X) #getting the hour
-
-# ID of the different sensors
-
-ID_cap=3
-ID_cap2=4
-
-# Subscription to building E
-
-abo_E209=$(mosquitto_sub -h mqtt.iut-blagnac.fr -t "Student/by-room/E209/data" -C 1)  
-temp_209=30
-hum_209=3
-date_209=$(date +%F)
-heure_209=$(date +%X)
-ID_cap_E209=1
-ID_cap_E209_2=2
-
-# Displaying the values obtained
-
-echo "Bâtiement B :" "température :" $temp "," "date et heure :" $date $heure  
-echo "Bâtiement B :" "humidité :" $hum "," "date et heure :" $date $heure  
-
-echo "Bâtiement E :" "température :" $temp_209 "," "date et heure :" $date_209 $heure_209  
-echo "Bâtiement E :" "humidité :" $hum_209 "," "date et heure :" $date_209 $heure_209  
-
-#Insertion of temperature and humidity measures for both building
-
-#Insertion for building B
-
- echo "INSERT INTO sae23.mesure (date, heure, type , valeur, \`id-capteur\`) VALUES ('$date', '$heure', '$temp', '$ID_cap');" | /opt/lampp/bin/mysql -h localhost -u bensaid -padnane85
-
- echo "INSERT INTO sae23.mesure (date, heure, type, valeur, \`id-capteur\`) VALUES ('$date', '$heure', '$hum', '$ID_cap2');" | /opt/lampp/bin/mysql -h localhost -u bensaid -padnane85
-
-#Insertion for building E 
-
-echo "INSERT INTO sae23.mesure (date, heure, type, valeur, \`id-capteur\`) VALUES ('$date_209', '$heure_209', '$temp_209', '$ID_cap_E209');" | /opt/lampp/bin/mysql -h localhost -u bensaid -padnane85
-
-echo "INSERT INTO sae23.mesure (date, heure, type, valeur, \`id-capteur\`) VALUES ('$date_209', '$heure_209', '$hum_209', '$ID_cap_E209_2');" | /opt/lampp/bin/mysql -h localhost -u bensaid -padnane85
-
-echo "Insertion des valeurs réalisée avec succès" # message to see if the insertion was successful
-
-
-
-#--------------------------------------SCRIPT DYNAMIQUE-----------------------------------------
-
-nbBatiments=$(echo "SELECT COUNT(\`id-batiment\`) FROM sae23.batiment;" | /opt/lampp/bin/mysql -h localhost -u bensaid -padnane85 | sed -n 2p)
+nbBatiments=$(echo "SELECT COUNT(\`id-batiment\`) FROM \`sae23\`.\`batiment\`;" | /opt/lampp/bin/mysql -h localhost -u bensaid -padnane85 | sed -n 2p) #finds how many buildings are in the "batiment" table
 
 echo "Il y a $nbBatiments batiment(s) dans la base de données"
 
-for (( i=0; i<$nbBatiments; i++ ))
+for (( i=0; i<$nbBatiments; i++ )) #loop that goes from 0 to the number of building minus one
+
 do
-bat=$(echo "SELECT nom FROM sae23.batiment LIMIT $i,1;" | /opt/lampp/bin/mysql -h localhost -u bensaid -padnane85 | sed -n 2p)
-echo "Batiment traite : $bat "
-mes=$(mosquitto_sub -h mqtt.iut-blagnac.fr -t "Student/by-room/$bat/data" -C 1)
-date=$(date +%F)
-heure=$(date +%X)
-temp=$(echo $mes | jq '.[0].temperature')
-hum=$(echo $mes | jq '.[0].humidity')
-echo "La temperature est de $temp°C"
-echo "L'humidite est de $hum g/m3"
-echo "INSERT INTO sae23.mesure (date, heure, type, valeur, \`id-capteur\`) VALUES ('$date', '$heure', '$type_temp' ,'$temp', '$idCap');" | /opt/lampp/bin/mysql -h localhost -u bensaid -padnane85
+
+bat=$(echo "SELECT nom FROM \`sae23\`.\`batiment\` LIMIT $i,1;" | /opt/lampp/bin/mysql -h localhost -u bensaid -padnae85 | sed -n 2p) #variable that contains the name of the building we will be accessing the measures of
+echo "Bâtiment traité : $bat "
+
+#mesure=$(mosquitto_sub -h mqtt.iut-blagnac.fr -t "Student/by-room/$bat/data" -C 1) #may be uncommented in order to use the IUT broker while it's up
+mesure=$(mosquitto_sub -h 127.0.0.1 -t "Student/by-room/$bat/data" -C 1) #used together with the broker.sh script for faster debugging
+
+idCap=$(echo "SELECT \`id-capteur\` FROM \`sae23\`.\`capteur\` JOIN \`sae23\`.\`batiment\` ON \`capteur\`.\`id-batiment\`=\`batiment\`.\`id-batiment\` WHERE \`batiment\`.\`nom\`=\"$bat\" LIMIT 0,1;" | /opt/lampp/bin/mysql -h localhost -u bensaid -padnane85 | sed -n 2p) #finds the ID of the sensor located in the current building
+echo "ID du capteur correspondant : $idCap"
+
+sujet=$(echo "SELECT type FROM \`sae23\`.\`capteur\` JOIN \`sae23\`.\`batiment\` ON \`capteur\`.\`id-batiment\`=\`batiment\`.\`id-batiment\` WHERE \`batiment\`.\`nom\`=\"$bat\" LIMIT 0,1 ;" | /opt/lampp/bin/mysql -h localhost -u bensaid -padnane85 | sed -n 2p) #finds what type of measures are made by the sensor (e.g. : temperature, humidity...)
+echo "La mesure relevée est : $sujet"
+
+date=$(date +%F) #current date on YYYY-MM-DD format
+heure=$(date +%X) #current hour on HH:MM:SS format
+valeur=$(echo $mesure | jq '.[0].'$sujet) #extracts the value from the JSON payload according to what type of sensor measured it
+
+echo "La valeur relevée est $valeur"
+
+echo "INSERT INTO sae23.mesure (\`date\`, \`heure\`, \`valeur\`, \`id-capteur\`) VALUES ('$date', '$heure', '$valeur', '$idCap');" | /opt/lampp/bin/mysql -h localhost -u bensaid -padnane85 #inserts the complete measure into the "mesure" table
+
+
+# Metrics
+
+avg_temp=$(echo "SELECT AVG(\`valeur\`) FROM sae23.mesure WHERE \`id-capteur\` = '$idCap';" | /opt/lampp/bin/mysql -h localhost -u bensaid -padnane85 | awk 'NR==2{printf "%.2f", $1}')
+    min_temp=$(echo "SELECT MIN(\`valeur\`) FROM sae23.mesure WHERE \`id-capteur\` = '$idCap';" | /opt/lampp/bin/mysql -h localhost -u bensaid -padnane85 | awk 'NR==2{print $1}')
+    max_temp=$(echo "SELECT MAX(\`valeur\`) FROM sae23.mesure WHERE \`id-capteur\` = '$idCap';" | /opt/lampp/bin/mysql -h localhost -u bensaid -padnane85 | awk 'NR==2{print $1}')
+
+
 done
 done
 
