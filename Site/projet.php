d3aa50a7437ea74e9f203b85cbd80f0f6b9bc64b
rayan2309 <133368635+rayan2309@users.noreply.github.com>
2023-06-09 19:11:38 +0200

    Add files via upload
    gantt nodered et grafana

--- Site/projet.php
+++ Site/projet.php
@@ -2,11 +2,27 @@
 <html lang="fr">
 	<head>
 		<meta charset="UTF-8" />
-		<title>Accueil SAE23</title>
+		<title>Gestion de projet</title>
 		<link rel="stylesheet" type="text/css" href="./styles/style.css" />
 	</head>
-	<body>
-		<p>Page de la gestion de projet</p>
+	<header>
+		<h2>Gestion de projet</h2>
+    </header>
+	<body>	
+		<h3>Diagramme de GANTT :</h3>
+		</br>
+		<p><img src="images/ganttfinal.png" alt="GANTT" width="75%" height="75%"></p>
+		<p>Diagramme de GANTT final</p>
+		</br>
+		<h3>Node-RED :</h3>
+		</br>
+		<p><img src="images/Livrable 2 et 3/Livrable 2/fluxnodered_SARRAT_BENSAID_ARROUD_SAE23.jpg" alt="Flux Node-RED" width="75%" height="75%"></p>
+		<p>Flux Node-RED</p>
+		</br>
+		<h3>Grafana :</h3>
+		</br>
+		<p><img src="images/Livrable 2 et 3/Livrable 3/dashboard_RT_grafana_SARRAT_BENSAID_ARROUD_SAE23.jpg" alt="Tableau de bord Grafana RT" width="75%" height="75%"></p>
+		<p>Tableau de bord Grafana des températures du bâtiment RT</p>
 		</br>
 		<p><a href="index.php">Retour a l'accueil</a></p>
 		</br>
