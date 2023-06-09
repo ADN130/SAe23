9defa5914fe7d4c896aa297500b86366246a0466
rayan2309 <133368635+rayan2309@users.noreply.github.com>
2023-06-09 19:18:53 +0200

    avec gantt nodered et grafana

--- Site/projet.php
+++ Site/projet.php
@@ -11,17 +11,17 @@
 	<body>	
 		<h3>Diagramme de GANTT :</h3>
 		</br>
-		<p><img src="images/ganttfinal.png" alt="GANTT" width="75%" height="75%"></p>
+		<p><img src="../Livrables/ganttfinal.png" alt="GANTT" width="75%" height="75%"></p>
 		<p>Diagramme de GANTT final</p>
 		</br>
 		<h3>Node-RED :</h3>
 		</br>
-		<p><img src="images/Livrable 2 et 3/Livrable 2/fluxnodered_SARRAT_BENSAID_ARROUD_SAE23.jpg" alt="Flux Node-RED" width="75%" height="75%"></p>
+		<p><img src="../Livrables/Livrable 2/fluxnodered_SARRAT_BENSAID_ARROUD_SAE23.jpg" alt="Flux Node-RED" width="75%" height="75%"></p>
 		<p>Flux Node-RED</p>
 		</br>
 		<h3>Grafana :</h3>
 		</br>
-		<p><img src="images/Livrable 2 et 3/Livrable 3/dashboard_RT_grafana_SARRAT_BENSAID_ARROUD_SAE23.jpg" alt="Tableau de bord Grafana RT" width="75%" height="75%"></p>
+		<p><img src="../Livrables/Livrable 3/dashboard_RT_grafana_SARRAT_BENSAID_ARROUD_SAE23.jpg" alt="Tableau de bord Grafana RT" width="75%" height="75%"></p>
 		<p>Tableau de bord Grafana des températures du bâtiment RT</p>
 		</br>
 		<p><a href="index.php">Retour a l'accueil</a></p>
