e051512d3de6eee65a8ee9ddde015ecc7b23048d
Benjamin Sarrat <benjamin.sarrat.iut@gmail.com>
2023-06-14 18:25:59 +0200

    remplacement des fichiers corrompus

--- Script/script_dyn.sh
+++ Script/script_dyn.sh
@@ -4,26 +4,26 @@
 
 nbBatiments=$(echo "SELECT COUNT(\`id-batiment\`) FROM \`sae23\`.\`batiment\`;" | /opt/lampp/bin/mysql -h localhost -u sarrat -ppassroot | sed -n 2p) #finds how many buildings are in the "batiment" table
 
-echo "Il y a $nbBatiments batiment(s) dans la base de données"
+echo "Il y a $nbBatiments batiment(s) dans la base de données" #displays how many buildings are in the database ("batiment" table)
 
-for (( i=0; i<$nbBatiments; i++ )) #loop that goes from 0 to the number of building minus one
+for (( i=0; i<$nbBatiments; i++ )) #loop that goes from 0 to the number of building not included
 do
 
-bat=$(echo "SELECT nom FROM \`sae23\`.\`batiment\` LIMIT $i,1;" | /opt/lampp/bin/mysql -h localhost -u sarrat -ppassroot | sed -n 2p) #variable that contains the name of the building we will be accessing the measures of
+bat=$(echo "SELECT \`id-batiment\` FROM \`sae23\`.\`batiment\` LIMIT $i,1;" | /opt/lampp/bin/mysql -h localhost -u sarrat -ppassroot | sed -n 2p) #variable that contains the name of the building we will be accessing the measures of
 echo "Bâtiment traité : $bat "
 
 #mesure=$(mosquitto_sub -h mqtt.iut-blagnac.fr -t "Student/by-room/$bat/data" -C 1) #may be uncommented in order to use the IUT broker while it's up
 mesure=$(mosquitto_sub -h 127.0.0.1 -t "Student/by-room/$bat/data" -C 1) #used together with the broker.sh script for faster debugging
 
-nbTypes=$(echo "SELECT COUNT(\`type\`) FROM \`sae23\`.\`capteur\` JOIN \`sae23\`.\`batiment\` ON \`capteur\`.\`id-batiment\`=\`batiment\`.\`id-batiment\` WHERE \`batiment\`.\`nom\`=\"$bat\";" | /opt/lampp/bin/mysql -h localhost -u sarrat -ppassroot | sed -n 2p)
+nbTypes=$(echo "SELECT COUNT(\`type\`) FROM \`sae23\`.\`capteur\` JOIN \`sae23\`.\`batiment\` ON \`capteur\`.\`id-batiment\`=\`batiment\`.\`id-batiment\` WHERE \`batiment\`.\`id-batiment\`=\"$bat\";" | /opt/lampp/bin/mysql -h localhost -u sarrat -ppassroot | sed -n 2p) #finds how many sensors are in the building currently processed
 
-for (( y=0; y<$nbTypes; y++ ))
+for (( y=0; y<$nbTypes; y++ )) #loop that goes from 0 to the number of sensors in the building unincluded
 do
 
-idCap=$(echo "SELECT \`id-capteur\` FROM \`sae23\`.\`capteur\` JOIN \`sae23\`.\`batiment\` ON \`capteur\`.\`id-batiment\`=\`batiment\`.\`id-batiment\` WHERE \`batiment\`.\`nom\`=\"$bat\" LIMIT $y,1;" | /opt/lampp/bin/mysql -h localhost -u sarrat -ppassroot | sed -n 2p) #finds the ID of the sensor located in the current building
+idCap=$(echo "SELECT \`id-capteur\` FROM \`sae23\`.\`capteur\` JOIN \`sae23\`.\`batiment\` ON \`capteur\`.\`id-batiment\`=\`batiment\`.\`id-batiment\` WHERE \`batiment\`.\`id-batiment\`=\"$bat\" LIMIT $y,1;" | /opt/lampp/bin/mysql -h localhost -u sarrat -ppassroot | sed -n 2p) #finds the ID of the sensor located in the current building
 echo "ID du capteur correspondant : $idCap"
 
-sujet=$(echo "SELECT type FROM \`sae23\`.\`capteur\` JOIN \`sae23\`.\`batiment\` ON \`capteur\`.\`id-batiment\`=\`batiment\`.\`id-batiment\` WHERE \`batiment\`.\`nom\`=\"$bat\" LIMIT $y,1;" | /opt/lampp/bin/mysql -h localhost -u sarrat -ppassroot | sed -n 2p) #finds what type of measures are made by the sensor (e.g. : temperature, humidity...)
+sujet=$(echo "SELECT type FROM \`sae23\`.\`capteur\` JOIN \`sae23\`.\`batiment\` ON \`capteur\`.\`id-batiment\`=\`batiment\`.\`id-batiment\` WHERE \`batiment\`.\`id-batiment\`=\"$bat\" LIMIT $y,1;" | /opt/lampp/bin/mysql -h localhost -u sarrat -ppassroot | sed -n 2p) #finds what type of measures are made by the sensor (e.g. : temperature, humidity...)
 echo "La mesure relevée est : $sujet"
 
 date=$(date +%F) #current date on YYYY-MM-DD format
@@ -37,4 +37,3 @@ echo "INSERT INTO sae23.mesure (\`date\`, \`heure\`, \`valeur\`, \`id-capteur\`)
 done
 
 done
-
---
+++ Site/admin/retrait_batiment.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Administration</title>
+		<link rel="stylesheet" type="text/css" href="../styles/style.css" />
+	</head>
+	<body>
+		<section>
+			<div class="centre">
+				<h1>Administrateur</h1>
+				<hr/>
+			</div>
+			<?php
+				session_start();
+				if ($_SESSION["login"]!="admin")
+						header("Location:login_error.php");
+				include ("mysql.php");
+				$requete = "SELECT nom FROM `batiment` ORDER BY `nom` DESC";
+				$resultat = mysqli_query($id_bd, $requete)
+					or die("Execution de la requete impossible : $requete");
+				mysqli_close($id_bd);
+
+				echo '<div class="tablo">';
+				while($ligne=mysqli_fetch_assoc($resultat))
+				 {	
+					extract($ligne);
+					echo '</br>';
+					echo '<ul>';
+					echo 	"<li> $nom </li>";
+					echo '</ul>';
+				 }
+				echo '</div>';
+			?>
+		<section>
+			<form action="retrait_batiment_script.php" method="post" enctype="multipart/form-data">
+		    	<div class="txt_field">
+		      	<input type="text" name="nom" id="nom" />
+		      	<span></span>
+		      	<label for="id">Batiment a retirer</label>
+		    	</div>
+				<input type="submit" value="Valider">
+      		</form>
+		</section>
+		<footer>
+		</footer>
+	</body>
+</html>
