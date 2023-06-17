d7af435e77674ff4ac278785c7883f7b9fdf99d0
ADN130 <133368769+ADN130@users.noreply.github.com>
2023-06-17 23:06:15 +0200

    Scripts dynamique version finale

---
+++ broker.sh
@@ -0,0 +1,37 @@
+
+#!/bin/bash
+
+#--------------------------------------BROKER DYNAMIQUE-----------------------------------------
+
+temp=${RANDOM:0:2} #generation of a random 2 digits integer (from 00 to 99)
+hum=${RANDOM:0:2}
+hum=${RANDOM:0:2}
+act=${RANDOM:0:2}
+co2=${RANDOM:0:2}
+tvoc=${RANDOM:0:2}
+illum=${RANDOM:0:2}
+infra=${RANDOM:0:2}
+visible=${RANDOM:0:2}
+pres=${RANDOM:0:2}
+
+payload=[{\"temperature\":$temp,\"humidity\":$hum,\"activity\":$act,\"co2\":$co2,\"tvoc\":$tvoc,\"illumination\":$illum,\"infrared\":$infra,\"infrared_and_visible\":$visible,\"pressure\":$pres},{\"deviceName\":\"AM107-37\",\"devEUI\":\"24e124128c019569\",\"room\":\"E208\",\"floor\":1,\"Building\":\"E\"}] #json payload that is going to be published, same format as the IUT broker
+
+
+
+nbBatiments=$(echo "SELECT COUNT(\`id-batiment\`) FROM \`sae23\`.\`batiment\`;" | /opt/lampp/bin/mysql -h localhost -u bensaid -padnane85 | sed -n 2p) #finds how many buildings are in the "batiment" table
+
+
+
+for (( i=0; i<$nbBatiments; i++ )) #loop that goes from 0 to the number of building minus one
+
+do
+
+bat=$(echo "SELECT \`id-batiment\` FROM \`sae23\`.\`batiment\` LIMIT $i,1;" | /opt/lampp/bin/mysql -h localhost -u bensaid -padnane85 | sed -n 2p) #finds which building i.e. which sensor is used (depending on room you chosed)
+echo $bat
+
+mosquitto_pub -h 127.0.0.1 -t Student/by-room/$bat/data -m $payload #publishes the JSON payload to the topic of the current building
+
+sleep 5 #waits for 2 seconds so that the mosquitto_sub can keep up
+
+done
+
---
+++ script_parallele.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# Indicating the path of the file to the crontab for the automatisation 
+
+cd "$(pwd)"
+
+# Recuperation of the MySQL database id from the arguments
+
+echo "Entrez votre identifiant PHPmyAdmin :"
+read user
+echo "Entrez votre mot de passe :"
+read password
+
+
+#-------------------------------------- SCRIPT PARALLELE -----------------------------------------
+
+nbBatiments=$(echo "SELECT COUNT(\`id-batiment\`) FROM \`sae23\`.\`batiment\`;" | /opt/lampp/bin/mysql -h localhost -u $user -p$password | sed -n 2p)
+
+echo "Il y a $nbBatiments batiment(s) dans la base de données"
+
+for (( i=0; i<$nbBatiments; i++ ))
+do
+
+bat=$(echo "SELECT \`id-batiment\` FROM \`sae23\`.\`batiment\` LIMIT $i,1;" | /opt/lampp/bin/mysql -h localhost -u $user -p$password | sed -n 2p)
+echo "Bâtiment traité : $bat "
+
+touch $bat.sh
+
+echo '#!/bin/bash
+
+
+# Using one of the both broker available according script execution time
+
+heure=$(date +%H)
+
+if [ $heure -ge 8 ] && [ $heure -lt 19 ]; then
+    broker="mqtt.iut-blagnac.fr"
+else
+    broker="127.0.0.1"
+fi
+
+mesure=$(mosquitto_sub -h $broker -t "Student/by-room/$bat/data" -C 1) 
+
+nbTypes=$(echo "SELECT COUNT(\`type\`) FROM \`sae23\`.\`capteur\` JOIN \`sae23\`.\`batiment\` ON \`capteur\`.\`id-batiment\`=\`batiment\`.\`id-batiment\` WHERE \`batiment\`.\`id-batiment\`=\"$bat\";" | /opt/lampp/bin/mysql -h localhost -u $user -p$password | sed -n 2p)
+
+for (( y=0; y<$nbTypes; y++ ))
+do
+
+#finds the ID of the sensor located in the current building
+
+idCap=$(echo "SELECT \`id-capteur\` FROM \`sae23\`.\`capteur\` JOIN \`sae23\`.\`batiment\` ON \`capteur\`.\`id-batiment\`=\`batiment\`.\`id-batiment\` WHERE \`batiment\`.\`id-batiment\`=\"$bat\" LIMIT $y,1;" | /opt/lampp/bin/mysql -h localhost -u $user -p$password | sed -n 2p) 
+
+echo "ID du capteur correspondant : $idCap"
+
+#finds what type of measures are made by the sensor (e.g. : temperature, humidity...)
+
+sujet=$(echo "SELECT type FROM \`sae23\`.\`capteur\` JOIN \`sae23\`.\`batiment\` ON \`capteur\`.\`id-batiment\`=\`batiment\`.\`id-batiment\` WHERE \`batiment\`.\`id-batiment\`=\"$bat\" LIMIT $y,1;" | /opt/lampp/bin/mysql -h localhost -u $user -p$password | sed -n 2p) 
+echo "La mesure relevée est : $sujet"' >> $bat.sh
+
+echo 'date=$(date +"%d-%m-%Y") #current date on DD-MM-YYYY format
+heure=$(date +%X) #current hour on HH:MM:SS format' >> $bat.sh
+
+printf 'valeur=$(echo $mesure | jq ' >> $bat.sh
+
+printf \' >> $bat.sh
+printf '.[0].' >> $bat.sh
+printf \' >> $bat.sh
+
+# extracts the value from the JSON payload according to what type of sensor measured it
+
+echo '$sujet) 
+
+echo La valeur relevée est $valeur' >> $bat.sh
+
+printf 'echo "INSERT INTO sae23.mesure (\`date\`, \`heure\`, \`valeur\`, \`id-capteur\`) VALUES (' >> $bat.sh
+
+# send data to the corresponding room file
+
+printf \' >> $bat.sh
+printf '$date' >> $bat.sh
+printf \' >> $bat.sh
+printf ', ' >> $bat.sh
+printf \' >> $bat.sh
+printf '$heure' >> $bat.sh
+printf \' >> $bat.sh
+printf ', ' >> $bat.sh
+printf \' >> $bat.sh
+printf '$valeur' >> $bat.sh
+printf \' >> $bat.sh
+printf ', ' >> $bat.sh
+printf \' >> $bat.sh
+printf '$idCap' >> $bat.sh
+printf \' >> $bat.sh
+echo ');" | /opt/lampp/bin/mysql -h localhost -u $user -p$password #inserts the complete measure into the "mesure" table' >> $bat.sh
+
+echo 'done' >> $bat.sh
+
+echo 'rm -- "$0"' >> $bat.sh
+
+chmod 777 $bat.sh
+
+var='$bat'
+sed -i "s/$var/$bat/g" $bat.sh
+
+./$bat.sh &
+
+done
